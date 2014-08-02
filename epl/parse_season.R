07108d200f416f9d06fb4b1566377c84af78ef1a
Alex K <akaluzny@gmail.com>
2014-08-02 14:15:20 -0500

    Sorted league table and changed variable names to mention time and not date

--- epl/parse_season.R
+++ epl/parse_season.R
@@ -6,24 +6,25 @@ LoadSeason <- function(filename = "EPL 13-14.csv") {
   #
   # Returns:
   #   Data frame with the following columns
-  #     Date.Time: Date and time of when the game began. Format is "%m/%d/%y %H:%M".
+  #     Date.Time: Date and time of when the game began
   #     Home.Team: Home team
   #     Away.Team: Away team
   #     Home.Score: Goals scored by the home team
   #     Away.Score: Goals scored by the away team
   
   games <- read.csv(filename)
+  # Convert to time from format in file
   games$Date.Time <- strptime(games$Date.Time, format = "%m/%d/%y %H:%M", tz = "GMT")
   games
 }
 
-BuildLeagueTable <- function(games, start.date = "initial", finish.date = "final") {
-  # Builds a league table from individual games, including only games in the provided date range
+BuildLeagueTable <- function(games, start.time = "initial", finish.time = "final") {
+  # Builds a league table from individual games, including only games in the provided range
   #
   # Args:
   #   games: Data frame of individual games
-  #   start.date: Only games from this date will be used to compute league table. Default or "initial" will start with the first game.
-  #   finish.date: Only games up to this date will be used to compute league table. Default or "final" will finish with the last game.
+  #   start.date: Only games from this time will be used to compute league table. Default or "initial" will start with the first game.
+  #   finish.date: Only games up to this time will be used to compute league table. Default or "final" will finish with the last game.
   #
   # Returns:
   #   Data frame with the following columns
@@ -55,26 +56,29 @@ BuildLeagueTable <- function(games, start.date = "initial", finish.date = "final
 
   # Assume all games take two hours
   two.hours <- as.difftime(2, units="hours")
-  if (start.date == "initial") {
+  if (start.time == "initial") {
     # Initial time should begin with the first game
-    start.date <- games$Date.Time[1]
+    start.time <- games$Date.Time[1]
   } else {
     # Ongoing games should not be included
-    start.date <- start.date - two.hours
+    start.time <- start.time - two.hours
   }
-  if (finish.date == "final") {
+  if (finish.time == "final") {
     # Final time should end with the last game
-    finish.date <- games$Date.Time[nrow(games)]
+    finish.time <- games$Date.Time[nrow(games)]
   } else {
-    #Ongoing games should not be included
-    finish.date <- finish.date - two.hours
+    # Ongoing games should not be included
+    finish.time <- finish.time - two.hours
   }
-  games <- games[games$Date.Time >= start.date & games$Date.Time <= finish.date, ]
+  # Select only games within the appropriate time interval
+  games <- games[games$Date.Time >= start.time & games$Date.Time <= finish.time, ]
   
+  # Table is equal to the number of teams
   table <- data.frame(Team = levels(games$Home.Team))
   by.home.team <- split(games, games$Home.Team)
   by.away.team <- split(games, games$Away.Team)
   
+  # Calculate all home results
   table$Home.Games <- sapply(by.home.team, nrow)
   table$Home.Wins <- sapply(by.home.team, function(x) sum(x[, 4] > x[, 5]))
   table$Home.Draws <- sapply(by.home.team, function(x) sum(x[, 4] == x[, 5]))
@@ -84,6 +88,7 @@ BuildLeagueTable <- function(games, start.date = "initial", finish.date = "final
   table$Home.Goal.Difference <- table$Home.Scored - table$Home.Allowed
   table$Home.Points <- table$Home.Wins * 3 + table$Home.Draws
   
+  # Calculate all away results
   table$Away.Games <- sapply(by.away.team, nrow)
   table$Away.Wins <- sapply(by.away.team, function(x) sum(x[, 4] < x[, 5]))
   table$Away.Draws <- sapply(by.away.team, function(x) sum(x[, 4] == x[, 5]))
@@ -93,6 +98,7 @@ BuildLeagueTable <- function(games, start.date = "initial", finish.date = "final
   table$Away.Goal.Difference <- table$Away.Scored - table$Away.Allowed
   table$Away.Points <- table$Away.Wins * 3 + table$Away.Draws
   
+  # Calculate all total results by adding home and away
   table$Games <- table$Home.Games + table$Away.Games
   table$Wins <- table$Home.Wins + table$Away.Wins
   table$Draws <- table$Home.Draws + table$Away.Draws
@@ -102,5 +108,6 @@ BuildLeagueTable <- function(games, start.date = "initial", finish.date = "final
   table$Goal.Difference <- table$Home.Goal.Difference + table$Away.Goal.Difference
   table$Points <- table$Home.Points + table$Away.Points
   
-  table
+  # Sort standings by points, goal difference, and then goals
+  table[order(table$Points, table$Goal.Difference, table$Scored, decreasing = TRUE), ]
 }
